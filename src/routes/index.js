3f625a054d2575c6c29394bfc38d3a17f53be718
Hieu Tran <hieutm211@gmail.com>
2022-01-03 17:55:18 +0700

    add Dockerfile, .dockerignore for deploy; add /health api for healthcheck

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,9 @@ route = (app) => {
   app.use('/api/suggest', suggestRouters);
   app.use('/api/video', videoRouters);
   app.use('/auth', authRouters)
+  app.get('/health', (req, res) => {
+    res.status(200).send('ok')
+  })
 }
 
 module.exports = route
